cebbc17e2c0f1870bc393719153337268e7407ce
Danilomartinsdeoliveira <danilooliveira.161103@gmail.com>
2026-03-30 21:28:02 -0300

    commit

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,10 +11,7 @@ namespace EleicaoBrasilApi.Data
     public class AppDbContext : DbContext
     {
         
-        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
-        {
-            
-        }
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options){}
 
         public DbSet<Candidato> Candidatos {get; set;}
 
